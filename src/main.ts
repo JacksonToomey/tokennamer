7bf77d42f88a397d3580097ab505e7fa643bfc56
Jackson Toomey <jacksontoomey@gmail.com>
2024-04-30 17:02:16 -0500

    fix bug (#1)

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,8 @@ const isToken = (item: Item) => {
 OBR.onReady(async () => {
   OBR.scene.onReadyChange(async (ready) => {
     if (!ready) return;
+    const role = await OBR.player.getRole();
+    if (role !== "GM") return;
     OBR.scene.items.onChange(async (items) => {
       const tokens = items.filter(isToken).filter((item) => {
         const meta = getItemMetadata(item);
@@ -58,9 +60,11 @@ OBR.onReady(async () => {
         names[token.id] = `${token.name} ${newCount === 1 ? "" : newCount}`;
         groups[token.name] = newCount;
       }
+      if (tokens.length < 1) return;
       OBR.scene.items.updateItems(
         tokens.map((t) => t.id),
         (items) => {
+          console.log("updating item");
           for (let item of items) {
             const meta = getItemMetadata(item);
             const name = names[item.id];
@@ -75,64 +79,3 @@ OBR.onReady(async () => {
     });
   });
 });
-//     await OBR.scene.items.updateItems(tokens, async (tokensToUpdate) => {
-//       // const sceneMeta = await getSceneMetadata();
-//       // const groups = sceneMeta.groups || {};
-//       const groups: Record<string, number> = {};
-//       console.log(tokensToUpdate);
-//       for (let token of tokensToUpdate) {
-//         // const count = groups[token.name] || 0;
-//         // token.text.plainText = `${token.name} ${count + 1}`;
-//         token.text.plainText = "foo";
-//         // groups[token.name] = count + 1;
-//       }
-//       console.log(groups);
-//       // const groups = tokensToUpdate.reduce((acc, token) => {
-//       //   const count = acc[token.name] || 0;
-//       //   token.text.plainText = `${token.name} ${count + 1}`;
-//       //   return {
-//       //     ...acc,
-//       //     [token.name]: count + 1,
-//       //   };
-//       // }, sceneMeta.groups || {});
-//       // console.log(groups);
-//     });
-//   });
-// });
-// OBR.scene.items.onChange((items) => {
-//   const tokens = items
-//     .filter((i) => i.layer === "CHARACTER")
-//     .filter((i) => i.type === "IMAGE")
-//     .filter((i) => {
-//       const meta = getItemMetadata(i);
-//       return !meta.tokenName;
-//     });
-//   setTimeout(async () => {
-//     OBR.scene.items.updateItems(tokens, async (itemsToUpdate) => {
-//       for (const item of itemsToUpdate) {
-//         item.text.plainText = "foo";
-//       }
-//     });
-//   }, 10);
-// OBR.scene.items.updateItems(
-//   tokens.map((t) => t.id),
-//   async (itemsToUpdate) => {
-//     for (const item of itemsToUpdate) {
-//       item.text.plainText = "foo";
-//     }
-//     // const sceneMeta = await getSceneMetadata();
-//     // const groups = itemsToUpdate.reduce((acc, item) => {
-//     //   const currentCount = acc[item.name] || 0;
-//     //   item.text.plainText = `${item.name} ${currentCount + 1}`;
-//     //   setItemMetadata(item, { tokenName: item.text.plainText });
-//     //   return {
-//     //     ...acc,
-//     //     [item.name]: currentCount + 1,
-//     //   };
-//     // }, sceneMeta.groups || {});
-//     // console.log(groups);
-//     // await setMetadata({ ...sceneMeta, groups: {} });
-//   }
-// );
-// });
-// });
